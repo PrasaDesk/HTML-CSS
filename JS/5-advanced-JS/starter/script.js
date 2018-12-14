cb591dd63be7ee31bcf2b5fb2ce801b785d72c25
AM-13 <prasad.progdesk@gmail.com>
2018-12-14 19:08:46 +0530

    Budget Application Delegates

--- JS/5-advanced-JS/starter/script.js
+++ JS/5-advanced-JS/starter/script.js
@@ -189,18 +189,28 @@ interViewQuestions('builder')('Rock');
 /**************************************
 * Clouser
 **************************************/
-
+/*
 function retirment(retiarmentAge){
     
     var a = ' year is remain for the Retiarment';
     console.log('Retiarment age :\t'+retiarmentAge);
     
     return function(YearOfBirth){
+        
+        if(YearOfBirth > 2018){
+            console.log('Invalid Year');
+            return 'Invalid';
+        }
+        
         var age = 2018 - YearOfBirth;
         document.querySelector('.age').innerHTML = age;
         console.log('Person Age : \t\t'+age);
         
         var ret = retiarmentAge - age;
+        
+        if(ret <= 0){
+            ret = 'Retired';
+        }
         
         return ret;
         //console.log((retiarmentAge - age) + a);
@@ -214,3 +224,167 @@ document.querySelector('.btn').addEventListener('click', function(){
     document.querySelector('.Retage').innerHTML = retiarmentUS(temp);
 });
 
+*/
+
+/*****************************************
+* Call, Bind And Apply
+*****************************************/
+/*
+// Call
+
+var person = {
+    name : 'jhon',
+    age : 22,
+    job : 'Developer',
+    presentation : function(style, greeting){
+        switch(style){
+            case 'formal':
+                console.log('Good '+ greeting +', Ladies and Gentalmans; '+'Hi this is '+ this.name +' i\'m a '+ this.job + ', my Age is '+ this.age);
+                break;
+            
+            case 'friendly':
+                console.log('Hey, Good '+ greeting +', brothers and sisters; '+'Hi this is '+ this.name +' i\'m a '+ this.job + ', my Age is '+ this.age);
+        }
+    }
+};
+
+var emily = {
+    name : 'Emily',
+    age : 18,
+    job : 'Musician'
+};
+
+person.presentation('formal', 'morning');
+
+person.presentation.call(emily, 'friendly', 'afternoon');
+
+//bind
+
+var friendly = person.presentation.bind(emily, 'formal');
+
+friendly('Evening');
+
+var arr= [2013, 2000, 1997, 1990, 1982];
+
+function ArrCalc(arr, fn){
+    var brr = [];
+    for(var i = 0; i < arr.length; i++){
+        brr.push(fn(arr[i]));
+    }
+    return brr;
+}
+
+function CalAge(el){
+    return 2018 - el;
+}
+
+function isFullAge(limit, el){
+    return el >= limit;
+}
+
+var Ages = ArrCalc(arr, CalAge);
+console.log(Ages);
+
+var FullAgeJapan = ArrCalc(Ages, isFullAge.bind(this, 20));
+var FullAgeIndia = ArrCalc(Ages, isFullAge.bind(this, 18));
+
+console.log('Japan  :\t'+ FullAgeJapan);
+console.log('India  :\t'+ FullAgeIndia);
+
+
+*/
+
+/************************************************
+* Coding Challange
+************************************************/
+
+(function(){
+    
+    var Questions = function(question, answers, correct){
+        this.question = question;
+        this.answers = answers;
+        this.correct = correct;
+    }
+
+    Questions.prototype.displayQuestion = function() {
+        console.log(this.question);
+        for(var i = 0; i < this.answers.length ; i++){
+            console.log(i +' : '+this.answers[i]);
+        }
+    }
+
+
+
+    Questions.prototype.CheckAns = function(ans, callback) {   //here Callback is Function (score() [keepscore])
+
+        var sc;
+
+        if(this.answers[ans] === this.correct){
+            console.log('Correct Amswer');
+            sc = callback(true);
+        }
+        else{
+            console.log('Wrong Answer');
+            sc = callback(false);
+        }
+        this.CheckScore(sc);
+    }
+
+
+
+
+    Questions.prototype.CheckScore = function(score){
+        console.log('Current Score is ' + score);
+        console.log('----------------------------------------');
+    }
+
+
+
+    var que1 = new Questions('2 * 2 = ? ', [4, 7, 8], 4);
+    var que2 = new Questions('10 / 2 = ? ', [50, 5, 12], 5);
+    var que3 = new Questions('10 ^ 10 = ? ', [342, 203, 100], 100);
+
+    var Question = [que1, que2, que3]
+
+
+    //clouser
+    function score() {
+        var sc = 0;
+        return function(correct){
+            if(correct){
+                sc += 2;
+            }
+            else{
+                sc -= 1;
+            }
+            return sc;
+        }
+    }
+
+    var keepScore = score();
+
+
+    document.querySelector('.btn').addEventListener('click', function(){
+        nextQuestion();
+    });
+
+
+    function nextQuestion(){
+
+        var n = Math.floor(Math.random() * Question.length);
+
+        Question[n].displayQuestion();
+
+        //var ans = prompt('Enter the Correct Option : ');
+
+        var ans = document.querySelector('.option').value
+
+        if(ans !== 'exit'){
+            Question[n] .CheckAns(parseInt(ans), keepScore); //Passing the function as argument    
+            nextQuestion();
+        }
+    }
+
+    nextQuestion();
+    
+})();
---
+++ JS/6-budgety/starter/app.js
@@ -0,0 +1,265 @@
+
+var BudgetController = (function(){
+
+    //constructor for Expense
+    var expense = function(id, desc, value){
+        this.id = id;
+        this.desc = desc;
+        this.value = value;
+    }
+
+    //constructor for Income
+     var income = function(id, desc, value){
+         this.id = id;
+         this.desc = desc;
+         this.value = value;
+     }
+
+     var calculateTotal = function (type) {
+        var sum = 0;
+        
+        data.allItem[type].forEach(function (cur) {  
+            sum += cur.value;
+        });
+
+        data.total[type] = sum;
+     }
+
+     //All data 
+     var data = {
+         allItem : {
+            inc : [],
+            exp : []
+         },
+         total : {
+            inc : 0,
+            exp : 0
+         },
+         budget : 0,
+         percentage : -1
+     }
+
+     return {
+         addItem : function (type, desc, value) {  
+             var newItem, ID;
+
+             //creating new ID for Item
+             if(data.allItem[type].length > 0){
+                ID = data.allItem[type][data.allItem[type].length - 1].id + 1
+             }
+             else{
+                 ID = 0;
+             }
+             
+             //create New Item with its Corresponding type
+             if(type === 'exp'){
+                 newItem = new expense(type, desc, value);
+             }
+             else if(type === 'inc'){
+                 newItem = new income(type, desc, value);
+             }
+
+             //Adding new Item to Corresponding Array
+             data.allItem[type].push(newItem);
+
+
+             //return the new item
+             return newItem;
+         },
+
+
+
+         calculateBudget : function () {  
+            calculateTotal('exp');
+            calculateTotal('inc');
+
+            data.budget = data.total.inc - data.total.exp;
+
+            if(data.total.inc > 0){
+                data.percentage = Math.round((data.total.exp / data.total.inc ) * 100);
+            }
+            else{
+                data.percentage = -1;
+            }           
+
+        },
+
+        getBudget : function(){
+            return {
+                budget : data.budget,
+                totalInc :data.total.inc,
+                totalExp : data.total.exp,
+                percentage : data.percentage
+            };
+        },
+
+         testing : function(){
+            console.log(data);
+        }
+     };
+
+    
+
+})();
+
+
+var UIController = (function () {  
+
+    var DOMString = {
+        type : '.add__type',
+        description : '.add__description',
+        value : '.add__value' ,
+        btn : '.add__btn',
+        income : '.income__list',
+        expense : '.expenses__list',
+        budgetDisplay : '.budget__value',
+        expenseDisplay : '.budget__expenses--value',
+        incomeDisplay : '.budget__income--value',
+        percentageDisplay : '.budget__expenses--percentage'
+    };
+
+    return {                            
+        getInput: function(){
+            return {
+                type : document.querySelector(DOMString.type).value,
+                description : document.querySelector(DOMString.description).value,
+                value : parseFloat(document.querySelector(DOMString.value).value)   
+            };
+        },
+
+        addListItem : function (obj, type) {  
+            var html;
+            var newHtml;
+            var element;
+
+            //create a note for display the expenses and income details
+            //sending HTML through thi string
+            if(type === 'inc'){
+                element = DOMString.income;
+                html = '<div class="item clearfix" id="income-%id%"> <div class="item__description">%desc%</div> <div class="right clearfix"> <div class="item__value">+ %val%</div> <div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"> </i></button> </div> </div> </div>';
+            }
+            else if(type == 'exp'){
+                element = DOMString.expense;
+                html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%desc%</div> <div class="right clearfix"><div class="item__value">- %val%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            }
+
+            //replacing data %id% in html code with (obj.id)
+            newHtml = html.replace('%id%', obj.id);
+            newHtml = newHtml.replace('%desc%', obj.desc);
+            newHtml = newHtml.replace('%val%', obj.value);
+
+            //sending the HTML code or render it on browser
+            document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+        },
+
+        clearFields : function (){
+            var field, fieldArr;
+
+            field = document.querySelectorAll(DOMString.description + ', ' + DOMString.value);
+
+            fieldArr = Array.prototype.slice.call(field);
+
+            fieldArr.forEach(function(cur, index, array) {
+                cur.value = "";
+            });
+
+            fieldArr[0].focus();
+        },
+
+        displayBudget : function (obj) {  
+            document.querySelector(DOMString.budgetDisplay).textContent = obj.budget;
+            document.querySelector(DOMString.incomeDisplay).textContent = obj.totalInc;
+            document.querySelector(DOMString.expenseDisplay).textContent = obj.totalExp;
+            
+            if(obj.percentage > 0){
+                document.querySelector(DOMString.percentageDisplay).textContent = obj.percentage + '%';
+            }
+            else{
+                document.querySelector(DOMString.percentageDisplay).textContent = '--';
+            }
+
+        },
+
+        getDOMStrings : function(){
+            return DOMString;
+        }
+    };   
+    
+})();
+
+
+//Global App Controller
+var controller = (function(budgetCtrl, UICtrl){
+
+    var setupEventListener = function () {  
+           
+        var DOMS = UICtrl.getDOMStrings();
+
+        //onClick event
+        document.querySelector(DOMS.btn).addEventListener('click', ctrlItemAdd);
+
+        //key press event (ENTER)
+        document.addEventListener('keypress', function (event) {  
+
+            if(event.keyCode === 13 || event.which === 13){
+                ctrlItemAdd();
+            }
+
+        });
+    }
+   
+
+    var updateBudget = function(){
+
+        budgetCtrl.calculateBudget();
+
+        var budget = budgetCtrl.getBudget();
+        console.log(budget);
+
+        UICtrl.displayBudget(budget);
+
+    }
+
+
+    var ctrlItemAdd = function () {  
+
+        var InputData, newInput;
+
+        console.log('key Press');
+        InputData = UICtrl.getInput();
+        
+        //fetching New Item From UI controller
+        //calling method budgetCtrl.addItem and passing required Data (type, desc, val)
+        
+
+        if(InputData.description !== '' && InputData.value !== 0){
+            newInput = budgetCtrl.addItem(InputData.type, InputData.description, InputData.value);
+            console.log(newInput);
+           
+            UICtrl.addListItem(newInput, InputData.type);
+           
+            UICtrl.clearFields();
+           
+            updateBudget();
+        }
+        
+
+        
+    }
+   
+    return {
+        init : function () {  
+        console.log('Application is Started..!!!');
+        UICtrl.displayBudget({
+            budget : 0,
+            totalInc :0,
+            totalExp : 0,
+            percentage : 0        
+        });
+        setupEventListener();
+        }
+    };
+
+})(BudgetController, UIController);
+
+controller.init();
